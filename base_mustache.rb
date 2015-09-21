e7e053c30aaebe90fc479a1b8e80308d95847959
k.hamada <k.hamada.1988@gmail.com>
2015-09-22 00:52:44 +0900

    Move to /view

--- base_mustache.rb
+++ base_mustache.rb
@@ -4,10 +4,12 @@ require_relative 'logging'
 require_relative 'memorizable'
 include Memoizable
 
+TEMPLATE_DIR = '/view'
+
 class BaseMustache < Mustache
   include Logging
 
-  self.template_path = File.dirname(__FILE__)
+  self.template_path = File.dirname(__FILE__) + TEMPLATE_DIR
 
   def initialize
     logger.info 'Initialize'
