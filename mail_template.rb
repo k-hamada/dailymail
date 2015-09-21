c7a9b6845cc21a0e8647a03392756042d830f61c
k.hamada <k.hamada.1988@gmail.com>
2015-09-21 13:48:21 +0900

    Add date in MailTemplate

--- mail_template.rb
+++ mail_template.rb
@@ -1,7 +1,14 @@
 require 'mustache'
+require 'date'
 
 class MailTemplate < Mustache
     self.template_path = File.dirname(__FILE__)
+
+    DATE_TEMPLATE = '%Y/%m/%d(%a)'
+
+    def date
+        Date.today.strftime(DATE_TEMPLATE)
+    end
 end
 
 if __FILE__ == $0
